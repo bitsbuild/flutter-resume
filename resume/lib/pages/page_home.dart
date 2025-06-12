43da5437debbdfb2b529de7f19eb2055640aa37d
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-12 23:56:46 +0530

    Home Page Structure SetUp And Development Started

--- resume/lib/pages/page_home.dart
+++ resume/lib/pages/page_home.dart
@@ -10,6 +10,22 @@ class PageHome extends StatefulWidget {
 class _PageHomeState extends State<PageHome> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.black);
+    return Container(
+      color: Colors.black,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text('1', style: TextStyle(color: Colors.white)),
+            Text('2', style: TextStyle(color: Colors.white)),
+            Text('3', style: TextStyle(color: Colors.white)),
+            Text('4', style: TextStyle(color: Colors.white)),
+            Text('5', style: TextStyle(color: Colors.white)),
+          ],
+        ),
+      ),
+    );
   }
 }
